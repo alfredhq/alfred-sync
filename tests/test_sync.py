8308e682cb0018635dfc8700efd957a1930e58da
Dima Kukushkin <dima@kukushkin.me>
2012-09-08 22:47:09 +0400

    Add dummy testcase and travis config

---
+++ tests/test_sync.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class AlfredSyncTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_five_eq_five(self):
+        self.assertEqual(1 + 4, 5)
+
+    def tearDown(self):
+        pass
